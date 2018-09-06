fce23a34a03ea82668fe4d28b20143ee845d40db
s.nikulitsa <s.nikulitsa@securitycode.ru>
2018-09-06 11:51:59 +0300

    It is working!

---
+++ README.md
@@ -0,0 +1,1 @@
+#DogtagPKI + JSCEP example
--- src/main/java/JscepSandbox.java
+++ src/main/java/JscepSandbox.java
@@ -1,8 +1,5 @@
 import org.bouncycastle.asn1.DERPrintableString;
 import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
-import org.bouncycastle.asn1.x509.KeyUsage;
-import org.bouncycastle.asn1.x509.X509Extension;
-import org.bouncycastle.cert.CertIOException;
 import org.bouncycastle.cert.X509CertificateHolder;
 import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
 import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
@@ -10,13 +7,15 @@ import org.bouncycastle.operator.ContentSigner;
 import org.bouncycastle.operator.OperatorCreationException;
 import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 import org.bouncycastle.pkcs.PKCS10CertificationRequest;
+import org.bouncycastle.pkcs.PKCS10CertificationRequestBuilder;
 import org.bouncycastle.pkcs.jcajce.JcaPKCS10CertificationRequestBuilder;
 import org.jscep.client.Client;
 import org.jscep.client.ClientException;
 import org.jscep.client.DefaultCallbackHandler;
 import org.jscep.client.EnrollmentResponse;
-import org.jscep.client.verification.ConsoleCertificateVerifier;
 import org.jscep.transaction.TransactionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.security.auth.x500.X500Principal;
 import java.math.BigInteger;
@@ -26,135 +25,115 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.cert.CertStoreException;
-import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
-import java.security.cert.X509CertSelector;
 import java.security.cert.X509Certificate;
 import java.util.Calendar;
-import java.util.Collection;
 import java.util.Date;
 
+/**
+ * Good guide for DogTag installation on CentOS: https://magnus-k-karlsson.blogspot.com/2017/11/installing-and-testing-dogtag.html
+ * SCEP enable: http://www.dogtagpki.org/wiki/SCEP_Setup
+ */
 public class JscepSandbox {
 
-    private final static String SCEP_DOGTAG = "http://172.17.115.15:8080/ca/cgi-bin/pkiclient.exe";
+    private final static Logger logger = LoggerFactory.getLogger(JscepSandbox.class);
+
+    private final static String RSA = "RSA";
+    private final static String SHA256withRSA = "SHA256withRSA";
+    private final static String SHA512withRSA = "SHA512withRSA";
+    private final static String SHA1withRSA = "SHA1withRSA";
+    private final static String MD5withRSA = "MD5withRSA";
+
+    private final static String SCEP_DOGTAG = "http://[enter_your_dogtag_host]:8080/ca/cgi-bin/pkiclient.exe";
 
     public static void main(String[] args) {
-
         try {
 
-            //SCEP CLIENT
             URL url = new URL(SCEP_DOGTAG);
             Client jscepClient = new Client(
                     url,
-                    new DefaultCallbackHandler(new ConsoleCertificateVerifier())
-            );
-//            logger.info("STRONGEST ALGORITHM = " + jscepClient.getCaCapabilities().getStrongestSignatureAlgorithm());
-
-//            //SELF-SIGNED
-//            KeyPair registererKeyPair = generateKeys("RSA", 1024);
-//            X500Principal issuer = new X500Principal("CN=self-signet, L=test, ST=test, C=RU");
-//            BigInteger serial = BigInteger.ONE;
-//            Calendar calendar = Calendar.getInstance();
-//            calendar.add(Calendar.DATE, -1);
-//            Date notBefore = calendar.getTime();
-//            calendar.add(Calendar.DATE, +2);
-//            Date notAfter = calendar.getTime();
-//            X500Principal requester = new X500Principal("CN=self-signet, L=test, ST=test, C=RU");
-//            JcaX509v3CertificateBuilder certBuilder = new JcaX509v3CertificateBuilder(
-//                    issuer,
-//                    serial,
-//                    notBefore,
-//                    notAfter,
-//                    requester,
-//                    registererKeyPair.getPublic()
-//            );
-//            certBuilder.addExtension(X509Extension.keyUsage, false, new KeyUsage(KeyUsage.digitalSignature));
-//
-//            JcaContentSignerBuilder certSignerBuilder = new JcaContentSignerBuilder("SHA1withRSA");
-//            ContentSigner certSigner = certSignerBuilder.build(registererKeyPair.getPrivate());
-//            X509CertificateHolder certHolder = certBuilder.build(certSigner);
-            X509Certificate requesterCert = getSelfSignedCert(
-                    "CN=test-cert, L=test, ST=test, C=RU",
-                    "RSA",
-                    1024,
-                    BigInteger.ONE
+                    //TODO: write something better
+                    new DefaultCallbackHandler(x509Certificate -> true)
             );
 
+            KeyPair requesterKeyPair = generateKeys(RSA, 1024);
+            X500Principal requesterIssuer = new X500Principal("CN=test.org, L=test, ST=test, C=UK");
+            BigInteger serial = BigInteger.ONE;
+            Calendar calendar = Calendar.getInstance();
+            calendar.add(Calendar.DATE, -1); // yesterday
+            Date notBefore = calendar.getTime();
+            calendar.add(Calendar.DATE, +2); // tomorrow
+            Date notAfter = calendar.getTime();
+            X500Principal requesterSubject = new X500Principal("CN=test.org, L=test, ST=test, C=UK");
 
-            //CSR
-            KeyPair keyPair = generateKeys("RSA", 1024);
-            PublicKey publicKey = keyPair.getPublic();
-            PrivateKey privateKey = keyPair.getPrivate();
+            X509Certificate requesterCert = generateSelfSignedCert(
+                    requesterIssuer, serial, notBefore, notAfter, requesterSubject, requesterKeyPair, SHA256withRSA
+            );
 
-            X500Principal requesterSubject = new X500Principal("CN=test-cert, L=Test, ST=tesT, C=RU");
+            KeyPair entityKeyPair = generateKeys(RSA, 1024);
+            // On dogtag host: /var/lib/pki/pki-tomcat/ca/conf/flatfile.txt field PWD
+            PKCS10CertificationRequest csr = buildCSR(requesterSubject, entityKeyPair, "11111");
 
-            JcaPKCS10CertificationRequestBuilder csrBuilder =
-                    new JcaPKCS10CertificationRequestBuilder(requesterSubject, publicKey);
+            EnrollmentResponse response = jscepClient.enrol(requesterCert, requesterKeyPair.getPrivate(), csr);
+            logger.info(response.isSuccess() + "");
 
-            DERPrintableString password = new DERPrintableString("11111");
-            csrBuilder.addAttribute(PKCSObjectIdentifiers.pkcs_9_at_challengePassword, password);
-
-            JcaContentSignerBuilder csrSignerBuilder = new JcaContentSignerBuilder("SHA512withRSA");
-            ContentSigner csrSigner = csrSignerBuilder.build(privateKey);
-
-            PKCS10CertificationRequest csr = csrBuilder.build(csrSigner);
-
-            Collection<? extends Certificate> certificates
-                    = jscepClient.getCaCertificate().getCertificates(new X509CertSelector());
-            X509Certificate certificate = (X509Certificate) certificates.stream().findFirst().get();
-
-            EnrollmentResponse enrol = jscepClient.enrol(certificate, privateKey, csr);
-
-//            //REQUEST
-//            logger.info("================================================================");
-////            EnrollmentResponse response = jscepClient.enrol(requesterCert, privateKey, csr);
-//            logger.info("----------------------------------------------------------------");
-////            CertStore certStore = response.getCertStore();
-//
-//            logger.info("FINISH");
-
-
-        } catch (NoSuchAlgorithmException |
-                OperatorCreationException |
-                MalformedURLException |
-                ClientException |
-                TransactionException |
-                CertIOException |
-                CertificateException |
-                CertStoreException e
+        } catch (MalformedURLException
+                | NoSuchAlgorithmException
+                | OperatorCreationException
+                | CertificateException
+                | TransactionException
+                | ClientException
+                e
         ) {
-//            logger.error(e.getMessage(), e);
+            logger.error(e.getMessage(), e);
         }
 
     }
 
-    private static X509Certificate getSelfSignedCert(String name, String instance, int keySize, BigInteger serial)
-            throws CertIOException, OperatorCreationException, NoSuchAlgorithmException, CertificateException {
-        KeyPair registererKeyPair = generateKeys(instance, keySize);
-        X500Principal issuer = new X500Principal(name);
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DATE, -1);
-        Date notBefore = calendar.getTime();
-        calendar.add(Calendar.DATE, +2);
-        Date notAfter = calendar.getTime();
-        X500Principal requester = new X500Principal(name);
-        JcaX509v3CertificateBuilder certBuilder = new JcaX509v3CertificateBuilder(
-                issuer,
-                serial,
-                notBefore,
-                notAfter,
-                requester,
-                registererKeyPair.getPublic()
+    private static PKCS10CertificationRequest buildCSR(
+            X500Principal entitySubject,
+            KeyPair entityKeyPair,
+            String password
+    ) throws OperatorCreationException {
+
+        PKCS10CertificationRequestBuilder csrBuilder =
+                new JcaPKCS10CertificationRequestBuilder(entitySubject, entityKeyPair.getPublic());
+        csrBuilder.addAttribute(
+                PKCSObjectIdentifiers.pkcs_9_at_challengePassword,
+                new DERPrintableString(password)
         );
-        certBuilder.addExtension(X509Extension.keyUsage, false, new KeyUsage(KeyUsage.digitalSignature));
+        JcaContentSignerBuilder csrSignerBuilder = new JcaContentSignerBuilder(SHA1withRSA);
+        ContentSigner csrSigner = csrSignerBuilder.build(entityKeyPair.getPrivate());
 
-        JcaContentSignerBuilder certSignerBuilder = new JcaContentSignerBuilder("SHA512withRSA");
-        ContentSigner certSigner = certSignerBuilder.build(registererKeyPair.getPrivate());
+        return csrBuilder.build(csrSigner);
+    }
+
+    private static X509Certificate generateSelfSignedCert(
+            X500Principal requesterIssuer,
+            BigInteger serial,
+            Date notBefore,
+            Date notAfter,
+            X500Principal requesterSubject,
+            KeyPair requesterKeyPair,
+            String algorithm
+    ) throws OperatorCreationException, CertificateException {
+
+        JcaX509v3CertificateBuilder certBuilder =
+                new JcaX509v3CertificateBuilder(
+                        requesterIssuer,
+                        serial,
+                        notBefore,
+                        notAfter,
+                        requesterSubject,
+                        requesterKeyPair.getPublic()
+                );
+        PrivateKey requesterPrivKey = requesterKeyPair.getPrivate(); // from generated key pair
+        JcaContentSignerBuilder certSignerBuilder = new JcaContentSignerBuilder(algorithm); // from above
+        ContentSigner certSigner = certSignerBuilder.build(requesterPrivKey);
         X509CertificateHolder certHolder = certBuilder.build(certSigner);
+        JcaX509CertificateConverter converter = new JcaX509CertificateConverter();
 
-        return new JcaX509CertificateConverter().getCertificate(certHolder);
+        return converter.getCertificate(certHolder);
     }
 
     private static KeyPair generateKeys(String instance, int keySize) throws NoSuchAlgorithmException {
